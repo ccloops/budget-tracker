c3e7cfb1c14c2c26fa9780dd6d92b74fff4416ae
Catherine Looper <loopercat@gmail.com>
2018-11-07 11:18:48 -0800

    auditing

--- budget/src/App.js
+++ budget/src/App.js
@@ -1,10 +1,47 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Table from 'react-table';
+import { Box, Flex } from 'grid-styled';
+
+const Wrapper = styled.div`
+  color: ${ ({ styleIsGood }) => styleIsGood ? 'cornSilk' : 'darkSalmon' };
+  background-color: ${ ({ isGood }) => isGood ? 'darkSalmon' : 'darkSlateGray' };
+  cursor: pointer;
+`;
+
+const StyledTable = styled(Table)`
+  color: pink;
+  width: 500px;
+  height: 500px;
+`
+const StyledBox = styled(Box).attrs({
+  width: [1, 1, .5, .5],
+  className: 'whatever'
+})`
+  background-color: pink;
+  height: 500px;
+`;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGood: true,
+    }
+  }
+
+  handleIsGood = () => this.setState({ isGood: !this.state.isGood })
+  
   render() {
+    const {
+      handleIsGood,
+      state: {isGood},
+    } = this;
     return (
-      <Fragment className="App">
-      </Fragment>
+      <Wrapper styleIsGood={isGood}>
+        <h1 onClick={handleIsGood}>Hello</h1>
+        <StyledBox></StyledBox>
+      </Wrapper>
     );
   }
 }
---
+++ budget/src/component/category-form/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+// const CategoryForm = ({}) => (
+
+// );
+
+// export default CategoryForm;
